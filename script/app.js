f927536ecbba6c4487274a6ee98b08dd485f1b54
Brant Han <brant.han@gmail.com>
2020-11-29 21:15:00 -0800

    dropdown menu on portfolio tab

---
+++ script/app.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+// $("#drop-port").hover(function () {
+//   $(".dropdown").slideToggle();
+// });
+// });
+
+$(document).ready(function () {
+  $("#drop-port").click(function () {
+    $(".dropdown").toggle();
+  });
+//   $("#drop-port").mouseleave(function () {
+//     $(".dropdown").hide(60000);
+//   });
+});
+
